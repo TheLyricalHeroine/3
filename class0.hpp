90a517183ee782dc92733b51d414877bc26dbd57
TheLyricalHeroine <64040291+TheLyricalHeroine@users.noreply.github.com>
2020-05-30 14:44:16 +0300

    Update class0.hpp

--- class0.hpp
+++ class0.hpp
@@ -4,7 +4,7 @@ class CVector0 : public CVector {
 	CVector0();
 	CVector0(vector<double> v1, int len1, string FileName1);
 	CVector0(int len2);
-	CVector0(const CVector0& other);
+	CVector0(const CVector& other);
 	
 	using CVector::operator=;
 	friend CVector0 operator+(CVector& first, CVector& second);
